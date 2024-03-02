8d6d8d4dd3775f31e0af799c3d266ddf689e7236
Jay Patel <jaykoladia@gmail.com>
2024-03-01 22:54:09 -0500

    Committing files

---
+++ CodeForces/Java 21/1030A | In Search of an Easy Problem/249235641.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0; i<n; i++){
+            int x = sc.nextInt();
+            if(x==1){
+                System.out.println("HARD");
+                return;
+            }
+        }
+        System.out.println("EASY");
+    }
+ 
+}
